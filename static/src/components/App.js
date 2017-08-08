7cd07285fd151eefc27f059128595ed257603b62
Brice Tessier Huort <tessierhuort@gmail.com>
2017-08-09 01:20:43 +0200

    handle loading feature

--- static/src/components/App.js
+++ static/src/components/App.js
@@ -17,38 +17,37 @@ class App extends Component {
     .then((response) => {
       const ndjson = response.data.split('\n').slice(0, -1)
       const json = ndjson.map((item, i) => JSON.parse(item))
-      this.setState({ emoticons: json })
+      this.setState({ emoticons: json, loading: false })
     })
   }
 
   _renderEmoticons() {
     return this.state.emoticons.map((emoticon, index) => {
       return (
-        <div key={index} className="emoticon">
-          <div>{emoticon.face}</div>
-          <div>id:{emoticon.id}</div>
-          <div>{emoticon.date}</div>
-          <div>price:{emoticon.price}</div>
-          <div>size:{emoticon.size}</div>
-        </div>
+          <div key={index} className="emoticon">
+            <div>{emoticon.face}</div>
+            <div>id:{emoticon.id}</div>
+            <div>{emoticon.date}</div>
+            <div>price:{emoticon.price}</div>
+            <div>size:{emoticon.size}</div>
+          </div>
       )
     })
   }
 
-  _renderLoading() {
-    //return <Loader />
-    return <div>Loading....</div>
-  }
-
   render() {
+
+    if (this.state.loading) {
+      return <Loader />
+    }
+
     return (
       <div className="App">
         <div className="App-header">
         </div>
-          <div className="emoticonsList">
-            <Loader />
-            { this.state.loading ? this._renderLoading() : this._renderEmoticons() }
-          </div>
+        <div className="emoticonsList">
+          { this._renderEmoticons() }
+        </div>
       </div>
     );
   }
--- static/src/utils/Loader.js
+++ static/src/utils/Loader.js
@@ -3,10 +3,10 @@ import ContentLoader, { Rect, Circle } from 'react-content-loader';
 
 const Loader = () => {
   return(
-    <ContentLoader height={140} speed={1} primaryColor={'#333'} secondaryColor={'#999'}>
-      <Circle x={195} y={30} radius={30} />
-      <Rect x={50} y={80} height={10} radius={5} width={300} />
-      <Rect x={75} y={100} height={10} radius={5} width={250} />
+    <ContentLoader height={120} speed={2} primaryColor={'#333'} secondaryColor={'#999'}>
+      <Circle x={195} y={30} radius={10} />
+      <Rect x={145} y={80} height={8} radius={4} width={100} />
+      <Rect x={160} y={100} height={8} radius={4} width={75} />
     </ContentLoader>
   )
 }
