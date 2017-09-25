446dc3f806a59da69377812fc92fae2ad62ebf9f
Brice Tessier Huort <tessierhuort@gmail.com>
2017-09-25 15:22:26 +0200

    install prettier

--- static/src/views/App.js
+++ static/src/views/App.js
@@ -4,15 +4,14 @@ import Header from '../components/header/index'
 import { Wrapper } from '../components/wrapper'
 
 class App extends Component {
-  render() {
-    return(
-      <Wrapper>
-        <Header />
-        <EmoticonList />
-      </Wrapper>
-
-    )
-  }
+	render() {
+		return (
+			<Wrapper>
+				<Header />
+				<EmoticonList />
+			</Wrapper>
+		)
+	}
 }
 
-export default App;
+export default App
